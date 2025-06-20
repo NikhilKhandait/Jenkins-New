abac4f61368ffe0c75a4bf2a337f379e1cc08bb1
nikhil7873 <khandaitnikhil9@gmail.com>
2025-06-20 20:00:57 +0530

    done

---
+++ Test.java
@@ -0,0 +1,14 @@
+package com.rays;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello java im nikhil here ");
+		System.out.println("test the jenkins file ");
+		
+		System.out.println( new Date());
+	}
+}
